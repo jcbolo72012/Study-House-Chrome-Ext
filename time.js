a4ba464e8f40b28f34a72081a0f70633db3d9a64
John Bolognino <jcbolo72012@gmail.com>
2023-02-26 18:46:29 -0500

    Basic outline of tab list

---
+++ time.js
@@ -0,0 +1,8 @@
+setInterval(
+    function(){
+        var date = new Date();
+        document.getElementById("clock").innerHTML=date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
+    }, 1000
+
+);
